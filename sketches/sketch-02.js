f9393580db02c7c51693b95e4e584ef9535bf6eb
Richard Washington <richard.washington@taxassist.co.uk>
2021-12-30 16:40:05 +0000

    :lipstick: Play with some gradients in the loop

--- sketches/sketch-02.js
+++ sketches/sketch-02.js
@@ -4,32 +4,49 @@ const settings = {
   dimensions: [ 1080, 1080 ]
 };
 
+const degToRad = (degrees) => {
+  return degrees / 180 * Math.PI;
+};
+
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    context.fillStyle = 'black';
+    context.fillStyle = 'rgb(0, 0, 0)';
 
-    const x = width * 0.5;
-    const y = height * 0.5;
-    const w = width * 0.3;
-    const h = height * 0.3;
+    const cx = width * 0.5;
+    const cy = height * 0.5;
+    let x, y;
 
-    context.save();
-    context.translate(x, y);
-    context.rotate(0.3);
+    const w = width * 0.01;
+    const h = height * 0.1;
 
-    context.beginPath();
-    context.rect(-w * 0.5, -h * 0.5, w, h);
-    context.fill();
-    context.restore();
+    const num = 12;
+    const radius = width * 0.3;
+    const gradientStep = 255 / num;
+    const colourStart = 0;
 
-    context.translate(100, 400);
+    for(let i = 0; i < num; i++) {
+      const slice = degToRad(360 / num);
+      const angle = slice * i;
 
-    context.beginPath();
-    context.arc(0, 0, 50, 0, Math.PI *2);
-    context.fill();
+      const color =  colourStart + (gradientStep * i);
+
+      context.fillStyle = 'rgb('+ color +','+ color +','+ color +')';
+
+      x = cx + radius * Math.sin(angle);
+      y = cy + radius * Math.cos(angle);
+
+      context.save();
+      context.translate(x, y);
+      context.rotate(-angle);
+  
+      context.beginPath();
+      context.rect(-w * 0.5, -h * 0.5, w, h);
+      context.fill();
+      context.restore();
+    }   
   };
 };
 
